15a754d557ad67ba43accfee2e25088f810ee163
Mohammad Mustakim <mdmustakim9752@gmail.com>
2026-07-24 02:07:27 +0530

    Create 📄 File 01: 01_hello_world.c

---
+++ 📄 File 01: 01_hello_world.c
@@ -0,0 +1,21 @@
+📄 File 01: 01_hello_world.c
+Concept: C Program ka basic structure aur screen par Output dikhana.
+
+💻 Code:
+C
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+🧠 Key Learnings:
+1. #include <stdio.h>: C language ka Toolbox (Header File). Iske bina printf kaam nahi karta.
+
+2. int main(): Program ka main Engine. Execution yahin se shuru hota hai.
+
+3. printf(): Screen par message print karne ke liye.
+
+4. \n: New Line (Enter key jaisa kaam karta hai).
+
+5. ; (Semicolon): Har command ka Full Stop.
